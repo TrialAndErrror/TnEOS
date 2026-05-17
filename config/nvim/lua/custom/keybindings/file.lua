5c520be2dcffd94c141d82c7d273ea78b69260db
Wade Green <thewadegreen@gmail.com>
2026-05-17 16:08:09 -0400

    Add markdown preview

--- config/nvim/lua/custom/keybindings/file.lua
+++ config/nvim/lua/custom/keybindings/file.lua
@@ -1,12 +1,18 @@
 -- File operations keybindings
-local wk = require 'which-key'
+local wk = require("which-key")
+
+local function split_in_glow()
+	local file = vim.fn.expand("%")
+	vim.cmd("vsplit | terminal glow " .. file)
+end
 
 -- Register keybindings with which-key (using wk.add method)
 wk.add({
-  { '<leader>f', group = 'File Actions' },
-  { '<leader>ff', '<cmd>Telescope find_files<cr>', desc = 'Find file' },
-  { '<leader>fr', '<cmd>Telescope oldfiles<cr>', desc = 'Recent files' },
-  { '<leader>fs', '<cmd>w<cr>', desc = 'Save file' },
-  { '<leader>fS', '<cmd>wa<cr>', desc = 'Save all' },
-  { '<leader>fe', '<cmd>e ~/.config/nvim/init.lua<cr>', desc = 'Edit config' },
+	{ "<leader>f", group = "File Actions" },
+	{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find file" },
+	{ "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "Recent files" },
+	{ "<leader>fs", "<cmd>w<cr>", desc = "Save file" },
+	{ "<leader>fS", "<cmd>wa<cr>", desc = "Save all" },
+	{ "<leader>fe", "<cmd>e ~/.config/nvim/init.lua<cr>", desc = "Edit config" },
+	{ "<leader>fp", split_in_glow, desc = "Markdown Preview" },
 })
