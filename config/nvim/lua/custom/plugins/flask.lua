c52d22b5df34e43b8c3519814ebdf0277467a8d0
Wade Green <thewadegreen@gmail.com>
2026-05-17 16:08:18 -0400

    Add flask template support

---
+++ config/nvim/lua/custom/plugins/flask.lua
@@ -0,0 +1,48 @@
+-- Flask / Jinja2 development support
+return {
+	-- Extend treesitter with Python and Jinja2 parsers + filetype detection
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = function(_, opts)
+			vim.list_extend(opts.ensure_installed, { "python", "jinja" })
+		end,
+		init = function()
+			vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+				group = vim.api.nvim_create_augroup("flask-filetypes", { clear = true }),
+				callback = function(ev)
+					local path = ev.file
+					-- .jinja2, .j2, .jinja extensions → htmldjango (Jinja2 syntax)
+					if path:match("%.jinja2$") or path:match("%.j2$") or path:match("%.jinja$") then
+						vim.bo[ev.buf].filetype = "htmldjango"
+						return
+					end
+					-- .html files inside a templates/ directory
+					if path:match("[/\\]templates[/\\].*%.html$") then
+						vim.bo[ev.buf].filetype = "htmldjango"
+					end
+				end,
+			})
+		end,
+	},
+
+	-- Install pyright + djlint via mason
+	-- mason-lspconfig's default handler automatically sets up pyright with blink.cmp capabilities
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		opts = function(_, opts)
+			opts.ensure_installed = opts.ensure_installed or {}
+			vim.list_extend(opts.ensure_installed, { "pyright", "djlint" })
+		end,
+	},
+
+	-- djlint formatter for Jinja2/HTML template files
+	{
+		"stevearc/conform.nvim",
+		opts = function(_, opts)
+			opts.formatters_by_ft = opts.formatters_by_ft or {}
+			opts.formatters_by_ft.htmldjango = { "djlint" }
+			opts.formatters_by_ft.jinja = { "djlint" }
+			opts.formatters_by_ft.jinja2 = { "djlint" }
+		end,
+	},
+}
