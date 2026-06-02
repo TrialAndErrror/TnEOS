b61f6760a88a40b83c0c4de18556dc5ec4a7224c
Wade Green <thewadegreen@gmail.com>
2026-06-02 01:31:18 -0400

    Allow for unknown hostname

--- steps/admin.sh
+++ steps/admin.sh
@@ -4,7 +4,7 @@ set -e
 source ./ui.sh
 
 # Get current system info (already set during Arch install)
-HOSTNAME=$(hostname)
+HOSTNAME=$(hostname 2>/dev/null || cat /etc/hostname 2>/dev/null || echo "unknown")
 USERNAME=$USER
 
 # --- Verify distro detection ---
