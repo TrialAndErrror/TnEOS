9c95d5653847cb25e343e43617c8b403a4106b9f
Wade Green <61675550+TrialAndErrror@users.noreply.github.com>
2026-06-13 21:46:19 -0400

    Update ToggleTerm config (#13)
    Co-authored-by: Wade Green <wade@openarchitectsk12.com>

--- config/nvim/lua/custom/keybindings/toggle.lua
+++ config/nvim/lua/custom/keybindings/toggle.lua
@@ -1,11 +1,13 @@
 -- Toggle keybindings
-local wk = require 'which-key'
+local wk = require("which-key")
 
 -- Register keybindings with which-key (using wk.add method)
 wk.add({
-  { '<leader>t', group = 'Toggle Actions' },
-  { '<leader>tn', '<cmd>set rnu!<cr>', desc = 'Toggle relative number' },
-  { '<leader>tl', '<cmd>set list!<cr>', desc = 'Toggle list chars' },
-  { '<leader>tw', '<cmd>set wrap!<cr>', desc = 'Toggle word wrap' },
-  { '<leader>tt', '<cmd>ToggleTerm<cr>', desc = 'Toggle terminal' },
+	{ "<leader>t", group = "Toggle Actions" },
+	{ "<leader>tn", "<cmd>set rnu!<cr>", desc = "Toggle relative number" },
+	{ "<leader>tl", "<cmd>set list!<cr>", desc = "Toggle list chars" },
+	{ "<leader>tw", "<cmd>set wrap!<cr>", desc = "Toggle word wrap" },
+	{ "<leader>tt", "<cmd>ToggleTerm direction=float<cr>", desc = "Toggle terminal" },
+	{ "<leader>tf", "<cmd>ToggleTerm direction=tab<cr>", desc = "Toggle terminal (fullscreen)" },
 })
+
